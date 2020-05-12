e4d34628837ef3e0441c2d749ba2aba0b06af407
Tristan Shi <tristanshi@foxmail.com>
2020-05-12 22:13:54 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,90 @@
+# Disaster Response Pipeline Project
+
+# DSND-Term2-P1-Disaster-Response-Pipelines
+Udacity Data Scientist Nanodegree - Term 2 - Project 1 - Disaster Response Pipelines
+
+### Table of Contents
+
+1. [Project Overview](#ProjectOverview)
+2. [Project Components](#ProjectComponents)
+3. [File Description](#FileDescription)
+4. [Requirements](#Requirements)
+5. [Instructions](#Instructions)
+
+## 1. <a name="ProjectOverview">Project Overview </a>
+The goal of the project is to apply data engineering skills to analyze the [Disaster Response Messages](https://www.figure-eight.com/dataset/combined-disaster-response-data/) dataset provided by Figure Eight, and build a web application that can help emergency workers analyze incoming messages and sort them into specific categories to speed up aid and contribute to more efficient distribution of people and other resources.
+
+### 2. <a name="ProjectComponents">Project Components </a>
+There are three components:
+
+### 2.1. ETL Pipeline
+This is a data cleaning pipeline, that:
+- Load Messages and Categories csv datasets 
+- Merge the two datasets
+- Clean the data
+- Save data into a SQLite database
+
+### 2.2. ML Pipeline
+This is a machine learning pipeline, that:
+- Loads data from the SQLite database
+- Splits the dataset into training and test sets
+- Builds a text processing and machine learning pipeline
+- Trains and tunes a model using GridSearchCV
+- Outputs results on the test set
+- Exports the final model as a pickle file
+
+### 2.3. Flask Web App
+A Web application that is built and run using Flask framework.
+
+## 3. <a name="FileDescription">Files  </a>
+<pre>
+D:\WORKSPACE
+|   README.md
+|   
++---app
+|   |   run.py              //Flask file to run the web application
+|   |   
+|   \---templates           //contains html file for the web application
+|           go.html
+|           master.html
+|           
++---data
+|       disasterMessage.db      // output of the ETL pipeline
+|       disaster_categories.csv  // datafile of all the categories
+|       disaster_messages.csv    // datafile of all the messages
+|       process_data.py          //ETL pipeline scripts
+|       
+\---models
+|       disasterClf.pickle       //machine learning model's best estimator prameters
+|       train_classifier.py      //machine learning pipeline scripts to train and export a classifier
+</pre>        
+
+
+- ETL Pipeline Preparation-zh.ipynb # notebook to process data
+- ML Pipeline Preparation-zh.ipynb # notebook to train classifier
+- README.md
+
+## 4. <a name="Requirements">Requirements </a>    
+
+All of the requirements are captured in requirements.txt.  
+```python
+pip install -r requirements.txt
+```
+
+## 5. <a name="Instructions">Instructions </a>
+1. Run the following commands in the project's root directory to process data and train model.
+
+    - Run the ETL pipeline to clean data and store it in a SQLite database
+		```python
+		python data/process_data.py data/disaster_messages.csv data/disaster_categories.csv data/disasterMessage.db
+		```
+    - Run the ML pipeline to trains and tunes a model using GridSearchCV, and save the model
+		```python    
+        python models/train_classifier.py data/disasterMessage.db models/disasterClf.pickle
+		```
+2. Run your web app with the following command in the app's directory.
+
+	python app/run.py
+
+3. Go to http://127.0.0.1:3001/
+
---
+++ models/train_classifier.py
@@ -0,0 +1,146 @@
+# !usr/bin/env python
+# -*- coding:utf-8 _*-
+"""
+@Author: Tristan SHI
+@Email: shizhendong@unionpayintl.com
+
+@File: train_classifier.py
+@Time: 2020/4/22 14:56
+@Motto：也许这就是人生吧
+"""
+
+import sys
+import pandas as pd
+import numpy as np
+from sqlalchemy import create_engine
+import re
+import pickle  # pickle模块
+
+import nltk
+
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
+
+from nltk import sent_tokenize, word_tokenize
+from nltk.stem import WordNetLemmatizer
+
+import sklearn
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline, FeatureUnion
+
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.metrics import classification_report
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import LinearSVC
+from sklearn.naive_bayes import GaussianNB
+from sklearn.model_selection import RepeatedKFold
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import GridSearchCV
+
+from sklearn.ensemble import RandomForestClassifier
+def load_data(database_filepath: str)->(np.array, np.array, pd.core.indexes.base.Index):
+    """
+    load data from database
+    :param database_filepath:
+    :return:
+     X: training set, text.
+     Y: training label
+     category_names: training label's columns'names
+    """
+    # load data from database
+    engine = create_engine('sqlite:////home/workspace/data/DisasterMessages.db')
+    df = pd.read_sql_table(database_filepath, engine)
+    X = np.array(df['message'])
+    Y = np.array(df[df.columns[5:]])
+    category_names = df.columns[5:]
+    return X, Y, category_names
+
+
+def tokenize(text:str) ->list :
+    """
+    clean text data: lower, remove stop words, Lemmatization
+    :param text: message text
+    :return: tokens
+    """
+    stop_words = nltk.corpus.stopwords.words('english')
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text.lower())
+    lemmatizer = WordNetLemmatizer()
+
+    tokens = word_tokenize(text)
+    tokens = ([lemmatizer.lemmatize(word) for word in tokens if word not in stop_words])
+    return tokens
+
+
+def build_model() ->sklearn.pipeline.Pipeline:
+    """
+    build pipline model
+    :return: model
+    """
+    model = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier(RandomForestClassifier()))
+    ])
+    return model
+
+
+def evaluate_model(model:sklearn.pipeline.Pipeline, X_test: np.array, Y_test: np.array, category_names:pd.core.indexes.base.Index):
+    """
+    :param model:
+    :param X_test:
+    :param Y_test:
+    :param category_names:
+    :return:
+    """
+    Y_pred = model.predict(X_test)
+    for i, col in enumerate(category_names):
+        print(col, classification_report(Y_pred[:, i], Y_test[:, i]))
+    return True
+
+
+def save_model(model:sklearn.pipeline.Pipeline, model_filepath:str):
+    """
+    :param model: 
+    :param model_filepath: 
+    :return: 
+    """
+    with open(model_filepath, 'wb') as f:
+        pickle.dump(model, f)
+    return True
+
+
+def main():
+    if len(sys.argv) == 3:
+        database_filepath, model_filepath = sys.argv[1:]
+        print('Loading data...\n    DATABASE: {}'.format(database_filepath))
+        X, Y, category_names = load_data(database_filepath)
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+
+        print('Building model...')
+        model = build_model()
+
+        print('Training model...')
+        model.fit(X_train, Y_train)
+
+        print('Evaluating model...')
+        evaluate_model(model, X_test, Y_test, category_names)
+
+        print('Saving model...\n    MODEL: {}'.format(model_filepath))
+        save_model(model, model_filepath)
+
+        print('Trained model saved!')
+
+    else:
+        print('Please provide the filepath of the disaster messages database ' \
+              'as the first argument and the filepath of the pickle file to ' \
+              'save the model to as the second argument. \n\nExample: python ' \
+              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
+
+
+if __name__ == '__main__':
+    main()
